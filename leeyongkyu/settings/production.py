c310078a6992d559cd343085aabc3cdff63c2db6
Taehwan <wlminimal@gmail.com>
2019-07-17 12:42:20 -0700

    edit allowed hosts

--- leeyongkyu/settings/production.py
+++ leeyongkyu/settings/production.py
@@ -10,5 +10,6 @@ except ImportError:
 
 env = environ.Env()
 
-ALLOWED_HOSTS = ['pure-shore-88696.herokuapp.com', 'easternpresbyterian.church', 'www.easternpresbyterian.church']
+#ALLOWED_HOSTS = ['pure-shore-88696.herokuapp.com', 'easternpresbyterian.church', 'www.easternpresbyterian.church']
 SECRET_KEY = env('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split(',')
